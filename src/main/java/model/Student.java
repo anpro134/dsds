05bf7ffd2a365cda0298bc9c7dba739b1bc3b719
DELL <DELL@DESKTOP-AKNF31D>
2022-09-20 10:14:08 +0700

    Git

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -11,4 +11,11 @@ package model;
 public class Student {
     private long id;
     private String name;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    
 }
